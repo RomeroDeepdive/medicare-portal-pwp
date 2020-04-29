df2790581e7ea0ad1768954f6ee58329644652a6
John Romero <Jromero326@cnm.edu>
2020-04-29 01:26:51 -0600

    cleaning up rows. added picture.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -48,48 +48,55 @@ To increase the reach of a new or established social media presence (as measured
             <section class="Goal for my PWP">
                 <div class="Goals">
                     <br>
-                    <strong>Goals:</strong>
+     <strong>Goals:</strong>
                     <br>
                     <br>
-                    <li>To have an interactive and easy site for people to get information and have someone local to work with</li>
-                    <li>To be able to receive new clients for Medicare, individual, small and large group insurance. </li>
-                    <li>Freelance. Be able to create other Independent Brokers a website that can process referrals.</li>
-                    <li>To be able to receive a job or internship for my coding and websites.</li>
-                    <li>My Overall goal is to be able to be a freelance web designer and Independent Insurance Broker</li>
+         <li>To have an interactive and easy site for people to get information and have someone local to work with</li>
+          <li>To be able to receive new clients for Medicare, individual, small and large group insurance. </li>
+         <li>Freelance. Be able to create other Independent Brokers a website that can process referrals.</li>
+          <li>To be able to receive a job or internship for my coding and websites.</li>
+           <li>My Overall goal is to be able to be a freelance web designer and Independent Insurance Broker</li>
 <br>
                     <br>
                     <br>
-                    <section class="About-Me"
-                             <div class="Persona"
+    <section class="About-Me"
+    <div class="Persona"
 <p>
     <strong>Persona:</strong>
+    <br>
+    <img src="oldman.jpeg">
 </p>
-                    <p>
-                    <strong>Name:</strong>
-                    William Snyder
-                    </p>
-                    <strong>Age:</strong>
-                    64
-                    </p>
-                    <strong>Job/Occupation:</strong>
-                    Coal Miner-Retiring in a month
-                    </p>
-                    <strong>Technology:</strong>
-                    William uses an old Samsung 6 and has microsoft windows 7 at his home desktop. He has never
-                    cared for technology and doesn't plan on upgrading.
-                    </p>
-                    <strong>Behaviors:</strong>
-                    William want's to make this wholeprocess easy for himself. He doesn't want to be
-                    taken advantage of or have time for a sales pitch. Get to the point and be honest.
+      <p>
+    <strong>Name:</strong>
+     William Snyder
+</p>
+    <strong>Age:</strong>
+ 64
+ </p>
+     <strong>Job/Occupation:</strong>
+      Coal Miner-Retiring in a month
+        </p>
+    <strong>Technology:</strong>
+    William uses an old Samsung 6 and has microsoft windows 7 at his home desktop. He has never
+     cared for technology and doesn't plan on upgrading.
+  </p>
+    <strong>Behaviors:</strong>
+ William want's to make this wholeprocess easy for himself. He doesn't want to be
+ taken advantage of or have time for a sales pitch. Get to the point and be honest.
 
-                    </p>
-                    <strong>Situation:</strong>
-William is retiring and is eligible for Medicare in a month. He already submitted his information into Socail Security
-                    to sign up for Medicare 2 months ago. He is now looking for his next steps and what type of plans
-                    are available for him and that are not going to cost him a lot of money.
-                    He wants to find answers easily, speak with someone in person and not on the phone. He's already
-                    been getting calls from people not in New Mexico.
-                    </p>
+      </p>
+    <strong>Situation:</strong>
+William is retiring and is eligible for Medicare in a month. He already submitted his information into Social Security
+to sign up for Medicare 2 months ago. He is now looking for his next steps and what type of plans
+ are available for him and that are not going to cost him a lot of money.
+ He wants to find answers easily, speak with someone in person and not on the phone.
+   company.
+       </p>
+        <p>
+       <strong>Frustrations:</strong>
+        He's already been getting calls from people not in New Mexico and that do not want to meet with him in person.
+        He fin'ds it very hard to locate some locally that can assist him and give him information on more than one insurance
+        <P>
 </header>
 
 
