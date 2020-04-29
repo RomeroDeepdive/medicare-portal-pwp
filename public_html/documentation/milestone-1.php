1cb50d9019febf831282b090b11869b73bc1698a
John Romero <Jromero326@cnm.edu>
2020-04-29 01:13:32 -0600

    Persona being updated/created

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -25,7 +25,8 @@ To increase the reach of a new or established social media presence (as measured
         <div class= 'Purpose'>
         <br>
     <strong>Purpose:</strong>
-<br>
+            <br>
+            <br>
             <li>Purpose of my PWP is to not only educate people (particulary Seniors) about their options for Medicare</li>
             <li>Being an Independent Insurance Broker allows me to be contracted by any Insurance carrier I please, which
             allows me to sign people up for a Medicare plan that best benefits them and their overall health.</li>
@@ -37,6 +38,8 @@ To increase the reach of a new or established social media presence (as measured
                 <div class="Demographics">
                 </div>
             <strong>Audience:</strong>
+                <br>
+                <br>
                 <li>Medicare is designed for individuals that are 65 years of age or older generally.</li>
                 <li>Medicare can be for anyone individual that has also been on disability benefits for 2 or more years</li>
                 <li>All ages can visit my site to get more information on Medicare, individual insurance, small group and large group</li>
@@ -44,12 +47,49 @@ To increase the reach of a new or established social media presence (as measured
 <br>
             <section class="Goal for my PWP">
                 <div class="Goals">
+                    <br>
                     <strong>Goals:</strong>
+                    <br>
+                    <br>
                     <li>To have an interactive and easy site for people to get information and have someone local to work with</li>
                     <li>To be able to receive new clients for Medicare, individual, small and large group insurance. </li>
-                    <li>Be able to add other Independent Brokers to the website that could benefit from a referral website</li>
-                    <li></li>
+                    <li>Freelance. Be able to create other Independent Brokers a website that can process referrals.</li>
+                    <li>To be able to receive a job or internship for my coding and websites.</li>
+                    <li>My Overall goal is to be able to be a freelance web designer and Independent Insurance Broker</li>
+<br>
+                    <br>
+                    <br>
+                    <section class="About-Me"
+                             <div class="Persona"
+<p>
+    <strong>Persona:</strong>
+</p>
+                    <p>
+                    <strong>Name:</strong>
+                    William Snyder
+                    </p>
+                    <strong>Age:</strong>
+                    64
+                    </p>
+                    <strong>Job/Occupation:</strong>
+                    Coal Miner-Retiring in a month
+                    </p>
+                    <strong>Technology:</strong>
+                    William uses an old Samsung 6 and has microsoft windows 7 at his home desktop. He has never
+                    cared for technology and doesn't plan on upgrading.
+                    </p>
+                    <strong>Behaviors:</strong>
+                    William want's to make this wholeprocess easy for himself. He doesn't want to be
+                    taken advantage of or have time for a sales pitch. Get to the point and be honest.
 
+                    </p>
+                    <strong>Situation:</strong>
+William is retiring and is eligible for Medicare in a month. He already submitted his information into Socail Security
+                    to sign up for Medicare 2 months ago. He is now looking for his next steps and what type of plans
+                    are available for him and that are not going to cost him a lot of money.
+                    He wants to find answers easily, speak with someone in person and not on the phone. He's already
+                    been getting calls from people not in New Mexico.
+                    </p>
 </header>
 
 
