f8271eff8c9e674a1b6bcefd904ec2a41f617449
John Romero <Jromero326@cnm.edu>
2020-04-29 01:52:51 -0600

    PWP phase 1 check in.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,38 +27,41 @@ To increase the reach of a new or established social media presence (as measured
     <strong>Purpose:</strong>
             <br>
             <br>
-            <li>Purpose of my PWP is to not only educate people (particulary Seniors) about their options for Medicare</li>
+            <li>My Purpose is to educate individuals (particulary Seniors) about their options for Medicare and other
+                insurance options available</li>
             <li>Being an Independent Insurance Broker allows me to be contracted by any Insurance carrier I please, which
             allows me to sign people up for a Medicare plan that best benefits them and their overall health.</li>
-            <li>This PWP I would like to be able to get referrals from people in New Mexico that would like for me to
+            <li>I would like to be able to get referrals from people in New Mexico that would like for me to
             come to their home, speak on the phone or virtually and help them sign up for Medicare.</li>
 <br>
-            <!-- Audience\/demogrphics of PWP Project Section-->
+            <!-- Audience/demogrphics of PWP Project Section-->
             <section class="Demographics for my PWP">
                 <div class="Demographics">
                 </div>
-            <strong>Audience:</strong>
+<strong>Audience:</strong>
                 <br>
                 <br>
-                <li>Medicare is designed for individuals that are 65 years of age or older generally.</li>
-                <li>Medicare can be for anyone individual that has also been on disability benefits for 2 or more years</li>
-                <li>All ages can visit my site to get more information on Medicare, individual insurance, small group and large group</li>
-            </section>
+    <li>All ages can visit my site to get more information on Medicare, individual insurance, small group and large group</li>
+    <li>Medicare is designed for individuals that are 65 years of age or older generally. However,</li>
+    <li>Medicare can be for any individual that has also been on disability benefits for 2 or more years</li>
+    <li>I would also assist those that cannot get health insurance through their work and have to pay for their own.</li>
+    <li>I can work with small and large group companies that are interested in getting their employee's health insurance.</li>
+     </section>
 <br>
-            <section class="Goal for my PWP">
+    <section class="Goal for my PWP">
                 <div class="Goals">
                     <br>
      <strong>Goals:</strong>
                     <br>
                     <br>
          <li>To have an interactive and easy site for people to get information and have someone local to work with</li>
-          <li>To be able to receive new clients for Medicare, individual, small and large group insurance. </li>
+         <li>To be able to receive new clients for Medicare, individual, small and large group insurance. </li>
          <li>Freelance. Be able to create other Independent Brokers a website that can process referrals.</li>
-          <li>To be able to receive a job or internship for my coding and websites.</li>
-           <li>My Overall goal is to be able to be a freelance web designer and Independent Insurance Broker</li>
+         <li>To be able to receive a job or internship for my coding and websites.</li>
+         <li>My Overall goal is to be able to be a freelance web designer and Independent Insurance Broker</li>
 <br>
-                    <br>
-                    <br>
+  <br>
+   <br>
     <section class="About-Me"
     <div class="Persona"
 <p>
@@ -66,7 +69,7 @@ To increase the reach of a new or established social media presence (as measured
     <br>
     <img src="oldman.jpeg">
 </p>
-      <p>
+   <p>
     <strong>Name:</strong>
      William Snyder
 </p>
@@ -75,28 +78,47 @@ To increase the reach of a new or established social media presence (as measured
  </p>
      <strong>Job/Occupation:</strong>
       Coal Miner-Retiring in a month
-        </p>
+ </p>
+   <p>
     <strong>Technology:</strong>
     William uses an old Samsung 6 and has microsoft windows 7 at his home desktop. He has never
      cared for technology and doesn't plan on upgrading.
-  </p>
+</p>
+     <p>
     <strong>Behaviors:</strong>
- William want's to make this wholeprocess easy for himself. He doesn't want to be
+ William want's to make this whole process easy for himself. He doesn't want to be
  taken advantage of or have time for a sales pitch. Get to the point and be honest.
-
-      </p>
+</p>
+    <p>
     <strong>Situation:</strong>
 William is retiring and is eligible for Medicare in a month. He already submitted his information into Social Security
 to sign up for Medicare 2 months ago. He is now looking for his next steps and what type of plans
  are available for him and that are not going to cost him a lot of money.
- He wants to find answers easily, speak with someone in person and not on the phone.
-   company.
-       </p>
+ He wants to find answers easily, speak with someone in person and not be on the phone all day.
+</p>
+    <p>
+     <strong>His Goals with my site:</strong>
+        <li>I want to be able to find some answer's without speaking with anyone and some resources</li>
+        <li>I want to be able to schedule an appointment for someone to call me and come to my home at my convenience</li>
+         <li>I want someone that can give me information on more than 1 insurance plan and the difference's for each.</li>
+ </p>
         <p>
        <strong>Frustrations:</strong>
-        He's already been getting calls from people not in New Mexico and that do not want to meet with him in person.
-        He fin'ds it very hard to locate some locally that can assist him and give him information on more than one insurance
-        <P>
+	      <li>He's already been getting calls from people <strong>not</strong> in New Mexico and that do not want to meet with him in person.</li>
+        <li>He finds it very hard to locate someone locally that can assist him and give him information on more than one insurance plan.</li>
+         <li>He Can't seem to find anyone that has answer's to his general questions.</li>
+         <li>He tries calling Social Security and the wait time is more than an hour and they are not helpful</li>
+</P>
+  <p>
+      <strong>What else is needed:</strong>
+      <li>He can fill out the form on my site to speak with an Independent Insurance Agent/Broker today</li>
+       <li>Please have your Medicare Care Card (Red, Wite and Blue Card) handy before your meeting</li>
+       <li>Where do you go to the hospital (if you ever have too) Presbyterian, Lovelace, UNM, V.A.?</li>
+       <li>Do you have any other concerns, beside your health care? Dental coverage, Vision</li>
+       <li>How often do you buy supplies at Walgreens, CVS or other convenient stores</li>
+        <li>Answering these basic questions will help your agent determine what plan(s) are best suited for you</li>
+
+ </p>
 </header>
 
 
