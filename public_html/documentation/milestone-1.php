bbf20b5a35063c2923d6d9ca2bd48249762a69b6
John Romero <Jromero326@cnm.edu>
2020-04-29 00:51:08 -0600

    audience and goals updated

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -20,21 +20,39 @@ To increase the reach of a new or established social media presence (as measured
 </head>
 <body>
 <header>
-    <setion class= 'Purpose of my project'>
+    <!-- Purpose of PWP Project Section-->
+    <section class= 'Purpose of my project'>
         <div class= 'Purpose'>
         <br>
-<strong>Purpose</strong>
+    <strong>Purpose:</strong>
 <br>
             <li>Purpose of my PWP is to not only educate people (particulary Seniors) about their options for Medicare</li>
             <li>Being an Independent Insurance Broker allows me to be contracted by any Insurance carrier I please, which
             allows me to sign people up for a Medicare plan that best benefits them and their overall health.</li>
-            <li>This PWP I would like to be able to get referrals from people in NEw MExico that would like for me to
+            <li>This PWP I would like to be able to get referrals from people in New Mexico that would like for me to
             come to their home, speak on the phone or virtually and help them sign up for Medicare.</li>
+<br>
+            <!-- Audience\/demogrphics of PWP Project Section-->
+            <section class="Demographics for my PWP">
+                <div class="Demographics">
+                </div>
+            <strong>Audience:</strong>
+                <li>Medicare is designed for individuals that are 65 years of age or older generally.</li>
+                <li>Medicare can be for anyone individual that has also been on disability benefits for 2 or more years</li>
+                <li>All ages can visit my site to get more information on Medicare, individual insurance, small group and large group</li>
+            </section>
+<br>
+            <section class="Goal for my PWP">
+                <div class="Goals">
+                    <strong>Goals:</strong>
+                    <li>To have an interactive and easy site for people to get information and have someone local to work with</li>
+                    <li>To be able to receive new clients for Medicare, individual, small and large group insurance. </li>
+                    <li>Be able to add other Independent Brokers to the website that could benefit from a referral website</li>
+                    <li></li>
 
 </header>
 
 
 </div>
-</setion>
 </body>
 </html>
