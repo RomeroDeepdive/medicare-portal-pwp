26d5c559a110bfd731d0994dbbbf932dd846ffc8
John Romero <Jromero326@cnm.edu>
2020-04-29 00:33:31 -0600

    Purpose of PWP.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,40 @@
+<!--standards compliant HTML-->
+<!--Purpose: What would you like your PWP to help you accomplish? This is a high-level goal akin to a Mission Statement.
+Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of?
+Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?
+Goal: This is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
+To acquire full or part-time employment or an internship.
+To increase sales and/or conversion rates for an existing business.
+To attract freelance clients.
+To serve as a gallery to showcase and promote your work.
+To increase the reach of a new or established social media presence (as measured via analytics tools). -->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+
+    <title>My PWP Project</title>
+    <br>
+</head>
+<body>
+<header>
+    <setion class= 'Purpose of my project'>
+        <div class= 'Purpose'>
+        <br>
+<strong>Purpose</strong>
+<br>
+            <li>Purpose of my PWP is to not only educate people (particulary Seniors) about their options for Medicare</li>
+            <li>Being an Independent Insurance Broker allows me to be contracted by any Insurance carrier I please, which
+            allows me to sign people up for a Medicare plan that best benefits them and their overall health.</li>
+            <li>This PWP I would like to be able to get referrals from people in NEw MExico that would like for me to
+            come to their home, speak on the phone or virtually and help them sign up for Medicare.</li>
+
+</header>
+
+
+</div>
+</setion>
+</body>
+</html>
