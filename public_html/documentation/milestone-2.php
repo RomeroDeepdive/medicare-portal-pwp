ba8c9cabf7df729b7ea1d2d7d5741181a4e44ae3
John Romero <Jromero326@cnm.edu>
2020-04-30 21:33:19 -0600

    about me section updated

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,17 +13,20 @@
 <section class="Welcome-Context">
 <div class="Welcome">
 	<p>Welcome:</p>
-	<li>This will be my background context (New Mexico img).</li>
+	<li>This will be my background context (Sandia Mountain img).</li>
 	<li>My personal Logo</li>
 	<li>Drop down feature</li>
+	<li>Links lead to other area's of my page</li>
 	<br>
 	<section class="About-Me"
 	         <div class="Personal-About-me"
 	<p>About me:</p>
 	<li>This section will detail my HealthCare Experience (13 years)</li>
+	<li>Detail of experience of past experience and serving our community.</li>
 	<li>I would like to give details about who I am, what I enjoy doing with my free time</li>
-	<li>Detail of experience of past experience and serving our community.\</li>
-	<li></li>
+	<li>I'd like to have some interactive photos come in and out about me.</li>
+	<li>img would include outdoors, family photo, me, photos of Abq and around NM</li>
+	<li>This would give people a chance to have some kind of connection with me before we even meet</li>
 </div>
 </section>
 <body>
