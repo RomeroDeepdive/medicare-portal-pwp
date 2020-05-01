49bbd5e22182968444caa7ab20d790ca68683223
John Romero <Jromero326@cnm.edu>
2020-04-30 21:52:34 -0600

    contact me-portfolio being worked on

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,24 +12,44 @@
 <p>Interactive features and functionality</p>
 <section class="Welcome-Context">
 <div class="Welcome">
-	<p>Welcome:</p>
-	<li>This will be my background context (Sandia Mountain img).</li>
-	<li>My personal Logo</li>
-	<li>Drop down feature</li>
-	<li>Links lead to other area's of my page</li>
+	<h2>Welcome:</h2>
+
+		<li>This will be my background context (Sandia Mountain img).</li>
+		<li>My personal Logo</li>
+		<li>Drop down feature</li>
+		<li>Links lead to other area's of my page</li>
 	<br>
 	<section class="About-Me"
-	         <div class="Personal-About-me"
-	<p>About me:</p>
-	<li>This section will detail my HealthCare Experience (13 years)</li>
-	<li>Detail of experience of past experience and serving our community.</li>
+	 <div class="Personal-About-me">
+	<h2>About me:</h2>
+
+	<li>This section will detail my HealthCare Experience (13 years), introduce myself as
+	an Independent insurance agent.</li>
+	<li>The current companies that I am representing/contracted with in NM.</li>
+	<li>Detail of experience of past experience and serving my community.</li>
 	<li>I would like to give details about who I am, what I enjoy doing with my free time</li>
 	<li>I'd like to have some interactive photos come in and out about me.</li>
 	<li>img would include outdoors, family photo, me, photos of Abq and around NM</li>
 	<li>This would give people a chance to have some kind of connection with me before we even meet</li>
 </div>
 </section>
-<body>
 
+<section class="Portfolio-"
+<br>
+<section class="Contact-me">
+<div class="contact-form-container">
+	<h2>Contact Me:</h2>
+	<li>This section will have radio buttons for what they'd like more information on
+	This will include dental, vision, hospitals they go too, and current plan.</li>
+	<li>I'd have an additional content box for additional information they'd
+		like to speak about.</li>
+	<li>This contact form would go directly to my e-mail.</li>
+	<li>My logo in a faded background</li>
+	<li>personal phone number</li>
+	<Li>professional e-mail address</Li>
+	<li>professional image</li>
+	<body>
+</div>
+</section>
 </body>
 </html>
