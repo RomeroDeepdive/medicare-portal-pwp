495aacfa10671f0173f3ba8f59a442fbb7b12141
John Romero <Jromero326@cnm.edu>
2020-04-30 21:19:25 -0600

    Phase 2 interactivty activity and interaction details

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+
+	<title>Milestone-2</title>
+</head>
+<br>
+<strong>Content Strategy</strong>
+<br>
+<p>Interactive features and functionality</p>
+<section class="Welcome-Context">
+<div class="Welcome">
+	<p>Welcome:</p>
+	<li>This will be my background context (New Mexico img).</li>
+	<li>My personal Logo</li>
+	<li>Drop down feature</li>
+	<br>
+	<section class="About-Me"
+	         <div class="Personal-About-me"
+	<p>About me:</p>
+	<li>This section will detail my HealthCare Experience (13 years)</li>
+	<li>I would like to give details about who I am, what I enjoy doing with my free time</li>
+	<li>Detail of experience of past experience and serving our community.\</li>
+	<li></li>
+</div>
+</section>
+<body>
+
+</body>
+</html>
