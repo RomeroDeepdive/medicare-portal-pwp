97c14615388f8ab2cba5f097bbaf17829c489210
John Romero <Jromero326@cnm.edu>
2020-05-22 08:14:44 -0600

    contact form and js.

---
+++ public_html/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+	$("#contact").validate({
+		debug: true,
+		errorClass: "alert alert-danger",
+		errorLabelContainer: "#output-area",
+		errorElement: "div",
+		//rules here define what is good and bad input
+		//each rule starts with the form input elements NAME attribute
+		rules: {
+			name: {
+				required:true
+			},
+			email: {
+				email: true,
+				required: true
+			},
+			message: {
+				require: true,
+				maxLength:2000
+			}
+		},
+		//error message to display to the end user when rules above don't pass
+
+		messages: {
+			name: {
+				required: "Please enter your name"
+			},
+			email: {
+				required: "Email is a requird field",
+				email: "Invalid Email"
+			},
+			message: {
+				required: "message is required",
+				maxlength: "You have exceeded the number of characters allowed"
+			}
+		},
+
+		submitHandler: function (form) {
+			$("#contact").ajaxSubmit({
+				type: "POST",
+				url: $("contact").attr("action"),
+				success: function (ajaxOutput) {
+					$("#output-area").css("display", "")
+					$("#output-area").html(ajaxOutput)
+
+					//reset the form if it was successful
+					if($(",alert-success").length>=1 ) {
+						$("contact")[0].reset();
+					}
+
+				}
+			})
+
+		}
+	})
+})
